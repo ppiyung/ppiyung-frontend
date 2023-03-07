01e5ba9b50fcd8d8d32a62f9ceef773c3d5b7af7
0tak2 <0tak2.code@gmail.com>
2023-03-07 20:03:57 +0900

    eslint 적용

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
+import Vue from 'vue';
+import axios from 'axios';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.prototype.$axios = axios
+Vue.prototype.$axios = axios;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount('#app');
