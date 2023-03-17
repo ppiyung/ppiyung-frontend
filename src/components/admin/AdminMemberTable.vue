70ab6e42fdf96e032d12f075c3f0572078eff46a
0tak2 <0tak2.code@gmail.com>
2023-03-17 09:03:30 +0900

    머지 전 커밋

--- src/components/admin/AdminMemberTable.vue
+++ src/components/admin/AdminMemberTable.vue
@@ -6,6 +6,31 @@
                 다운로드
                 </b-button>
             </template> -->
+
+            <template #cell(show_details)="row">
+                <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+                상세정보 {{ row.detailsShowing ? '닫기' : '보기'}}
+                </b-button>
+            </template>
+
+            <template #row-details="row"> 
+                <b-card>
+                    <b-row class="mb-2">
+                        <b-col sm="3" class="text-sm-right"><b>지원자 거주지 :</b></b-col>
+                        <b-col>{{ row.item.memberAddr }}</b-col>
+                    </b-row>
+
+                    <b-row class="mb-2">
+                        <b-col sm="3" class="text-sm-right"><b>지원자 연락처 :</b></b-col>
+                        <b-col>{{ row.item.memberPhone}}</b-col>
+                    </b-row>
+
+                    <b-row class="mb-2">
+                        <b-col sm="3" class="text-sm-right"><b>지원자 이메일 :</b></b-col>
+                        <b-col>{{ row.item.memberEmail}}</b-col>
+                    </b-row>
+                </b-card>
+            </template>
         </b-table>
  
         <div class="page-indicator">
@@ -78,12 +103,19 @@ export default {
                     label: '회원 승인 여부',
                     sortable: true
                 },
+                {
+                    key: 'show_details',
+                    label: '상세 정보'
+                }
             ]
         }
     },
     computed: {
         memberList() {
-            return this.$store.getters['admin/memberManage'].memberList;
+            return this.$store.getters['admin/memberManage'].memberList
+                .map(item => {
+                    return {...item, _showDetails: false};
+                });
         },
         total() {
             return this.$store.getters['admin/memberManage'].total;
